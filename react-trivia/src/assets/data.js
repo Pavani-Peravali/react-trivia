3f8a7389223c0b58a27c63dd460be2bd5f82211e
Pavani Peravali <pavaniperavali7@gmail.com>
2024-03-29 16:19:16 -0400

    react quiz app code

---
+++ react-trivia/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "Biofilter technology is used to process what?",
+        option1: "Gas",
+        option2: "Water",
+        option3: "Sand",
+        option4: "Wool",
+        ans: 2
+    },
+    {
+        question: "Life's Good' is the slogan of which tech company?",
+        option1: "LG",
+        option2: "Samsung",
+        option3: "Onida",
+        option4: "Sony",
+        ans: 1
+    },
+    {
+        question: "A file ending .au features which content?",
+        option1: "Text",
+        option2: "Video",
+        option3: "Audio",
+        option4: "Excel",
+        ans: 3
+    },
+    {
+        question: "A location for storing files on your computer has which name?",
+        option1: "Directory",
+        option2: "File",
+        option3: "Dictionary",
+        option4: "Drive",
+        ans: 1
+    },
+    {
+        question: "Amazon was founded in which US state?",
+        option1: "Georgia",
+        option2: "NewYork",
+        option3: "California",
+        option4: "Washington",
+        ans: 4
+    },
+];
